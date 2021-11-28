c93476275b6f3ea921fa46eda558f5c8c081f2e4
tiagovianez <tiagovianez@live.com>
2021-11-28 01:26:30 -0300

    Aula 02

--- aula2.py
+++ aula2.py
@@ -15,19 +15,24 @@ print(divisao)
 print(multiplicacao)
 print(resto)
 
+
 #--- IDENTIFICANDO QUAL O TIPO DA VARIÁVEL ---
 print(type(soma))
+
 
 #--- CONVERTENDO UMA VARIÁVEL DO TIPO INTEIRO PARA STRING ---
 soma: str = str(soma)
 print(type(soma))
 
+
 #--- CONCATENAÇÃO DE VARIÁVEIS ---
 print('soma: ' + str(soma))
+
 
 #--- Cálculos ---
 print (soma)
 print (subtracao)
+
 
 # --- CONVERTENDO PARA TIPO 'string' ---
 print ('subtracao: ' + str(subtracao))
@@ -35,13 +40,16 @@ print (multiplicacao)
 print(divisao)
 print(resto)
 
+
 # # --- ARREDONDANDO UM VALOR DE ACORDO COM A CONDIÇÃO 'int' ---
 print(int(divisao))
+
 
 # ---- DETERMINANDO UMA VARIÁVEL DO TIPO 'int' PARA EFETUAR CÁLCULO ---
 x = '7'
 soma2 = int(x) + 1
 print(soma2)
+
 
 #--- INTERAÇÃO COM O USUÁRIO | DETERMINADO VARIAVEL INTEIRA PARA INSERIR VALORES NO INPUT ---
 a = int(input('Entre com o primeiro valor: '))
@@ -52,6 +60,7 @@ subtracao = a - b
 multiplicacao = a * b
 divisao = a / b
 resto = a % b
+
 
 # --- COMANDO 'format' | CONCATENAÇÃO INDEPENDENTE DE UMA 'float' OU 'string' ---
 print('soma: {sm}. '
