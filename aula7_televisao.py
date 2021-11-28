8ad1e267eeda4e4b0fe3ac933d2b4411817c0d60
tiagovianez <tiagovianez@live.com>
2021-11-28 01:52:50 -0300

    Aula 07

--- aula7_televisao.py
+++ aula7_televisao.py
@@ -1,10 +1,10 @@
 # --- IDENTIFICANDO SE UMA TV ESTÁ LIGADA OU DESLIGADA | MUDANÇA DE CANAL ---
 class Televisao:
-    #variável inicial | Inicialmente a TV está desligada
+    #Inicialmente a TV está desligada
     def __init__(self):
         self.ligada = False
         self.canal  = 5
-    #ligando a TV caso esteja desligada
+    #Ligando a TV caso esteja desligada
     def power(self):
         if self.ligada:
             self.ligada = False
@@ -23,19 +23,19 @@ class Televisao:
 if __name__ == '__main__':
     televisao = Televisao()
     print('Televisao ligada: {}'.format(televisao.ligada))
-    #ligando a TV
+    #Ligando a TV
     televisao.power()
     print('Televisao ligada: {}'.format(televisao.ligada))
-    #desligando a TV
+    #Desligando a TV
     televisao.power()
     print('Televisao ligada: {}'.format(televisao.ligada))
-    #aumentando o canal 2x | Caso a TV esteja ligada
+    #Aumentando o canal 2x | Caso a TV esteja ligada
     televisao.power()
     print('Canal: {}'.format(televisao.canal))
     televisao.aumenta_canal()
     televisao.aumenta_canal()
     print('Canal: {}'.format(televisao.canal))
-    #diminuindo o canal 1x
+    #Diminuindo o canal 1x
     televisao.diminui_canal()
     print('Canal: {}'.format(televisao.canal))
 
