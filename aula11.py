7f7d0d5ce48a55f310f0d38e9cf0d35f8a950790
tiagovianez <tiagovianez@live.com>
2021-11-28 00:09:10 -0300

    aula 12

---
+++ aula11.py
@@ -0,0 +1,21 @@
+# --- FORÇANDO UM ERRO ---
+lista = [1, 10]
+try:
+    arquivo = open('teste.txt', 'r')
+    texto = arquivo.read()
+    divisao = 10 / 1
+    numero = lista[1]
+except ZeroDivisionError:
+    print('Não é possível realizar divisão por 0')
+except ArithmeticError:
+    print('Houve um erro ao realizar uma operação aritmética')
+except IndexError:
+    print('Erro ao acessar um indice inválido da lista')
+except Exception as ex:
+    print('Erro desconhecido. Erro: {}'.format(ex))
+else:
+    print('Executa quando não ocorre exceção')
+finally:
+    print('Sempre executa')
+    print('Fechando o arquivo')
+    arquivo.close()
---
+++ aula12.py
@@ -0,0 +1,29 @@
+import requests
+
+def retorna_dados_cep(cep):
+    response = requests.get('https://viacep.com.br/ws/01001000/json/'.format(cep))
+    print(response.status_code)
+    #--- FORMATO DE DICIONÁRIO | ACESSANDO DETERMINADA INFORMAÇÃO ---
+    print(response.json())
+    print(type(response.json()))
+    dados_cep = response.json()
+    print(dados_cep['localidade'])
+    print(dados_cep['bairro'])
+    return dados_cep
+
+def retorna_dados_pokemon(pokemon):
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon))
+    dados_pokemon = response.json()
+    return  dados_pokemon
+
+#--- FAZENDO LEITURA DE PÁGINAS PELO PYTHON ---
+def retorna_response(url):
+    response = requests.get(url)
+    return response.text
+
+if __name__ == '__main__':
+    response = retorna_response('https://www.quantumblack.com/careers')
+    print(response)
+    # retorna_dados_cep('01001000')
+    # dados_pokemon = retorna_dados_pokemon('pikachu')
+    # print(dados_pokemon)
